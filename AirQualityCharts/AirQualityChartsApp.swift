bd1c7c62d4e7603bada15d70cdd217dfd0d9d80f
ttttmmmm <junque135@gmail.com>
2025-04-24 16:59:26 -0400

    Initial Commit

---
+++ AirQualityCharts/AirQualityChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityChartsApp.swift
+//  AirQualityCharts
+//
+//  Created by antonio morales on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirQualityChartsTests/AirQualityChartsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityChartsTests.swift
+//  AirQualityChartsTests
+//
+//  Created by antonio morales on 4/24/25.
+//
+
+import Testing
+@testable import AirQualityCharts
+
+struct AirQualityChartsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
