b9904608ffffe636533dc419157258502748875d
ttttmmmm <junque135@gmail.com>
2025-05-27 20:18:03 -0400

    trying to get last sample on the view

---
+++ Views/RealTimeListenerSubViews/LastSampleView.swift
@@ -0,0 +1,18 @@
+//
+//  LastSampleView.swift
+//  AirQualityCharts
+//
+//  Created by antonio morales on 5/27/25.
+//
+
+import SwiftUI
+
+struct LastSampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LastSampleView()
+}
